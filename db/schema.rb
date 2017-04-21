405d4bcf574aaa8d4c6122a72c85374a3b7a09a3
yuyamashita <yu.yamashita@bibo.com.ph>
2017-04-21 17:38:57 +0800

    scaffold order_item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170422080136) do
+ActiveRecord::Schema.define(version: 20170422080137) do
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer  "order_id",   null: false
+    t.integer  "menu_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer  "user_id"
