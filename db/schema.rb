0eeca145c3662ed9d283f205691182898d55afa7
jop <joefrey.ramos@bibo.com.ph>
2017-04-21 22:59:54 +0800

    front-end design

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421081305) do
+ActiveRecord::Schema.define(version: 20170422080135) do
+
+  create_table "restaurants", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.string   "location"
+    t.float    "delivery_charge"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -32,12 +41,4 @@ ActiveRecord::Schema.define(version: 20170421081305) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "restaurants", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "name"
-    t.string   "location"
-    t.float    "delivery_charge"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
 end
