1ac52c5c2b10f47e79b836f5c1b191cc27f2d56b
yuyamashita <yu.yamashita@bibo.com.ph>
2017-04-21 17:30:34 +0800

    run migration

--- db/migrate/20170422080136_create_orders.rb
+++ db/migrate/20170422080136_create_orders.rb
@@ -1,7 +1,9 @@
 class CreateOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :orders do |t|
-
+      t.integer :user_id
+      t.integer :restaurant_id
+      t.integer :status
       t.timestamps
     end
   end
